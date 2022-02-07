d6cd1aade83576402a8ebfe0b95d78dd3fd3de41
Madhu-Rockstar <99171528+Madhu-Rockstar@users.noreply.github.com>
2022-02-07 14:00:15 +0530

    Add files via upload

---
+++ Assignment/Assignment/FebinossiNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class FebinossiNumber
+    {
+        static void Main()
+        {
+            febinossi(1000);
+        }
+        
+        public static void febinossi(int num)
+        {
+            
+            int a = 0;
+            int b = 1;
+            int res = a + b;
+            Console.Write(" "+a);
+            Console.Write(" "+b);
+            while (res<num)
+            {
+                
+                Console.Write(" " + res);
+                a = b;
+                b = res;
+                res = a + b;
+            }
+        }
+    }
+}
---
+++ Assignment/Assignment/House.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            SmallApartment apartment = new SmallApartment();
+            Person person = new Person("Madhu");
+            apartment.door = new House.Door("Merun Red");
+            person.house = apartment;
+            person.ShowData();
+            Console.ReadLine();
+        }
+
+
+        public class House
+        {
+            public int Area { get; set; }
+            public Door door;
+
+            public House(int area = 200)
+            {
+                Area = area;
+            }
+
+            public void ShowData()
+            {
+                Console.WriteLine($" my area is square feet of {Area} Square feet");
+            }
+
+            public Door GetDoor()
+            {
+                return door;
+            }
+
+            public class Door
+            {
+                public string Color { get; set; }
+
+                public Door(string color = "white")
+                {
+                    Color = color;
+                }
+
+                public void ShowData()
+                {
+                    Console.WriteLine($" my door color is {Color}");
+                }
+            }
+        }
+
+        public class SmallApartment : House
+        {
+            public SmallApartment(int area = 50) : base(area)
+            {
+            }
+        }
+
+        public class Person
+        {
+            private string name;
+            public House house { get; set; }
+
+            public Person(string name)
+            {
+                this.name = name;
+            }
+
+            public void ShowData()
+            {
+                Console.WriteLine($"A person whose name is {name}");
+                
+                house.ShowData();
+                
+                house.GetDoor().ShowData();
+            }
+
+        }
+    }
+}
